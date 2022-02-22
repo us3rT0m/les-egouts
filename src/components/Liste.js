0185054e0cfc3a4f3eee226121982e056b6444d5
us3rT0m <92308471+us3rT0m@users.noreply.github.com>
2022-02-22 10:28:23 +0100

    fix(App.js | components/Liste.js): fix image renderer when image is uploaded.

--- src/components/Liste.js
+++ src/components/Liste.js
@@ -1,10 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 
 const Liste = (props) => {
 
-    const [image, setImage] = useState({ preview: "", raw: "" });
+    const [image, setImage] = useState({preview:'',raw:''});
     const refInput = useRef();
+
+    // useEffect(()=>{
+    //     setImage
+    // }, [])
+
     const handleChange = e => {
 
         if (e.target.files.length) {
@@ -12,8 +17,8 @@ const Liste = (props) => {
                 preview: URL.createObjectURL(e.target.files[0]),
                 raw: e.target.files[0]
             });
-
-            props.onChangePic(image);
+            props.setUploaded([...props.uploaded, e.target.files[0]]);
+            console.log(e.target.files[0])  // print file uploaded in console
         }
     };
 
@@ -26,24 +31,32 @@ const Liste = (props) => {
 
     return (
         <div>
-        
-        <input
-            type="file"
-            id="upload-button"
-            style={{ display: "none" }}
-            onChange={handleChange}
-            ref={refInput}
-        />
-        <br />
-        <button onClick={handleUpload}>Upload</button>
 
-        <div>
-            <ul>
-                <li>
-                    <img src={image.preview} alt="dummy" width="300" height="300" />
-                </li>
-            </ul>
-        </div>
+            <input
+                type="file"
+                id="upload-button"
+                style={{ display: "none" }}
+                onChange={handleChange}
+                ref={refInput}
+            />
+            <br />
+            <button onClick={handleUpload}>Upload</button>
+
+            <div>
+                <ul>
+                    {props.uploaded.length > 0 && 
+                    props.uploaded.map(
+                        (x,index) => {
+                            return (
+                                <li key={index}>
+                                    <p>{x.name}</p>
+                                    <img src={URL.createObjectURL(x)} width="300" height="300" />
+                                </li>
+                            );
+                        }
+                    )}
+                </ul>
+            </div>
         </div>
     );
 }
